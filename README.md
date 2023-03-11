f1db0a8035e466ef47b4b922c7ee432c1f4ab366
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-03-11 17:06:17 +0800

    :sparkles: Added ability to parse github url

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 With just one click, you can easily convert any markdown file to a PDF document!
 
-Backend from https://dillinger.io/
+Markdown to PDF conversion is done using https://dillinger.io/
 
 ![try it image](https://imgur.com/zX1YnFQ.png)
 [![Download as PDF Button](https://img.shields.io/badge/Download%20AS%20pdf-EF3939?style=for-the-badge&logo=adobeacrobatreader&logoColor=white&color=black&labelColor=ec1c24)](https://mdtopdf.up.railway.app/convertPdf?url=https://raw.githubusercontent.com/iqfareez/badge-mdtopdf/master/README.md)
@@ -13,6 +13,7 @@ Copy the badges link:
 ```text
 [![Download as PDF Button](https://img.shields.io/badge/Download%20AS%20pdf-EF3939?style=for-the-badge&logo=adobeacrobatreader&logoColor=white&color=black&labelColor=ec1c24)](https://mdtopdf.up.railway.app/convertPdf?url=<url>)
 ```
-Replace the **`url`** with your **RAW** markdown file url.
+Replace the **`url`** with your GitHub's markdown file url. You can also supply raw file URL.
+Not tested with other universities.
 
 And finally, put the badge to your README file.
--- routes/convertPdf.js
+++ routes/convertPdf.js
@@ -4,8 +4,34 @@ const stream = require('stream');
 const {promisify} = require('util');
 const fetch = require('node-fetch');
 const {URLSearchParams} = require('url');
+const url = require('url');
 
 const pipeline = promisify(stream.pipeline);
+
+/// handle GitHub link. If it is not raw link, convert it to raw link first
+
+function githubLinkChecker(ghUrl)
+{
+    // just skip if it already github raw link
+    if (ghUrl.includes('raw.githubusercontent.com')) return ghUrl;
+
+    // skip if it is not github link
+    if (!ghUrl.includes("github.com")) return ghUrl;
+
+    // Parse the URL using the "url.parse" method
+    const parsedUrl = url.parse(ghUrl, true);
+
+    // Extract the username, repository name, branch name, and file path from the parsed URL
+    const owner = parsedUrl.pathname.split("/")[1];
+    const repo = parsedUrl.pathname.split("/")[2];
+    const branch = parsedUrl.pathname.split("/")[3];
+    const filePath = parsedUrl.pathname.slice(parsedUrl.pathname.indexOf(branch) + branch.length + 1);
+
+    console.log(branch)
+    console.log(filePath)
+
+    return `https://raw.githubusercontent.com/${owner}/${repo}/${filePath}`;
+}
 
 /* Convert Markdown to PDF. */
 router.get('/', async function (req, res, next) {
@@ -13,11 +39,15 @@ router.get('/', async function (req, res, next) {
     const url = req.query.url;
     // const url = 'https://raw.githubusercontent.com/iqfareez/mcte4342-embedded-system-design/main/Week11/README.md';
 
+    const fileUrl = githubLinkChecker(url);
+
+    console.log(fileUrl)
+
     let mdResponse;
 
     try {
-        if (!url) throw new Error('url is required');
-        mdResponse = await fetch(url);
+        if (!fileUrl) throw new Error('url is required');
+        mdResponse = await fetch(fileUrl);
     } catch (error) {
         res.render('error', {message: 'Error: ' + error.message, error: error})
         return
